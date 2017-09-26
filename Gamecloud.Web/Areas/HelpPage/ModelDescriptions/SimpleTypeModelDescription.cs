5b95dcdb9bc2b9bfbe2c22088c9fdf7224d81634
DEVOPS\Jason <romeroagency@outlook.com>
2017-09-26 00:45:57 -0400

    Add project files.

---
+++ Gamecloud.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Gamecloud.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Gamecloud.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gamecloud.Web.MvcApplication" Language="C#" %>
